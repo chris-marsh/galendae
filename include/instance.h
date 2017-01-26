4c77edf562fcbcd23a6137e9ccca91e15bd5c4c5
Chris Marsh <chris.pm@gmail.com>
2017-01-26 22:59:32 +0000

    Tidy up

--- include/instance.h
+++ include/instance.h
@@ -1,7 +1,25 @@
+/******************************************************************************
+ *                                                                            *
+ *                galandae copyright (c) 2016 Chris Marsh                     *
+ *               <https://github.com/chris-marsh/gcalendar                    *
+ *                                                                            *
+ * this program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the gnu general public license as published by the      *
+ * free software foundation, either version 3 of the license, or any later    *
+ * version.                                                                   *
+ *                                                                            *
+ * this program is distributed in the hope that it will be useful, but        *
+ * without any warranty; without even the implied warranty of merchantability *
+ * or fitness for a particular purpose.  see the gnu general public license   *
+ * at <http://www.gnu.org/licenses/> for more details.                        *
+ *                                                                            *
+ ******************************************************************************/
+
+
 #ifndef INSTANCE_H
 #define INSTANCE_H
 
-#define PID_FILENAME "/tmp/test_pid.lock"
+#define PID_FILENAME "/tmp/galendae_pid.lock"
 
 typedef struct Instance {
     int unique;
--- src/instance.c
+++ src/instance.c
@@ -1,3 +1,21 @@
+/******************************************************************************
+ *                                                                            *
+ *                galandae copyright (c) 2016 Chris Marsh                     *
+ *               <https://github.com/chris-marsh/gcalendar                    *
+ *                                                                            *
+ * this program is free software: you can redistribute it and/or modify it    *
+ * under the terms of the gnu general public license as published by the      *
+ * free software foundation, either version 3 of the license, or any later    *
+ * version.                                                                   *
+ *                                                                            *
+ * this program is distributed in the hope that it will be useful, but        *
+ * without any warranty; without even the implied warranty of merchantability *
+ * or fitness for a particular purpose.  see the gnu general public license   *
+ * at <http://www.gnu.org/licenses/> for more details.                        *
+ *                                                                            *
+ ******************************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
